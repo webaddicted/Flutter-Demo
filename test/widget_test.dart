9ec037ddab8fdca55cbef7c8b78719c70d07fcf1
Deepak Sharma <Anish@9950>
2020-05-02 23:55:03 +0530

    Social login complated like Google +, facebook and twitter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutterbeginner/view/main/main.dart';
+import 'package:flutterbeginner/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
