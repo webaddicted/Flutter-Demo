173133c5f7c291704be182b6042d9a70427dbb2d
Deepak Sharma <Anish@9950>
2020-05-06 00:08:25 +0530

    firbase all task complete

---
+++ android/app/src/main/kotlin/com/example/flutterbeginner/AppApplication.kt
@@ -0,0 +1,21 @@
+package com.example.flutterbeginner
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+/**
+ * Created by Deepak Sharma(Webaddicted) on 05-05-2020.
+ */
+class AppApplication : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        GeneratedPluginRegistrant.registerWith(registry)
+    }
+
+}
